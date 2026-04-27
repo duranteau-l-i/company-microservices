ba20805247f5f54f750f07796a3b8e570c4462d6
Ludovic <duranteau.l.i@gmail.com>
2026-04-27 07:32:49 +0100

    refactor(company-service): remove OfficerClient and OfficerQueryPort

--- company-service/src/main/java/com/company/companyservice/infrastructure/feign/OfficerCompanyLinkDto.java
+++
@@ -1,8 +0,0 @@
-package com.company.companyservice.infrastructure.feign;
-
-import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
-
-import java.util.UUID;
-
-@JsonIgnoreProperties(ignoreUnknown = true)
-public record OfficerCompanyLinkDto(UUID companyId, String title) {}

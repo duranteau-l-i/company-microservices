50d15ff055bbb314bdd5de2e31e43afccffbcf42
Ludovic <duranteau.l.i@gmail.com>
2026-05-29 15:27:55 +0100

    fix(user-service): prevent MANAGER from updating ADMIN or other MANAGER accounts
    (cherry picked from commit 0d1f0a22df81724ecb4364039fd61d9231d11138)

--- user-service/src/test/java/com/company/userservice/unit/application/command/UpdateUserHandlerTest.java
+++ user-service/src/test/java/com/company/userservice/unit/application/command/UpdateUserHandlerTest.java
@@ -64,4 +64,52 @@ class UpdateUserHandlerTest {
 
         assertThat(result.firstName()).isEqualTo("Renamed");
     }
+
+    @Test
+    void managerCannotUpdateAdminAccount() {
+        User admin = User.create(EmailAddress.of("admin@test.com"), "h", "Ad", "Min", Role.ADMIN).user();
+        repo.save(admin);
+        User manager = User.create(EmailAddress.of("mgr@test.com"), "h", "M", "Gr", Role.MANAGER).user();
+        repo.save(manager);
+
+        assertThatThrownBy(() -> handler.update(new UpdateUserUseCase.Command(
+                manager.id(), Role.MANAGER, admin.id(), "Hacked", "Admin")))
+                .isInstanceOf(InsufficientPermissionException.class);
+    }
+
+    @Test
+    void managerCannotUpdateAnotherManagerAccount() {
+        User manager1 = User.create(EmailAddress.of("mgr1@test.com"), "h", "M1", "Gr", Role.MANAGER).user();
+        repo.save(manager1);
+        User manager2 = User.create(EmailAddress.of("mgr2@test.com"), "h", "M2", "Gr", Role.MANAGER).user();
+        repo.save(manager2);
+
+        assertThatThrownBy(() -> handler.update(new UpdateUserUseCase.Command(
+                manager1.id(), Role.MANAGER, manager2.id(), "Hacked", "Manager")))
+                .isInstanceOf(InsufficientPermissionException.class);
+    }
+
+    @Test
+    void managerCanUpdateUserAccount() {
+        User manager = User.create(EmailAddress.of("mgr@test.com"), "h", "M", "Gr", Role.MANAGER).user();
+        repo.save(manager);
+
+        UserReadModel result = handler.update(new UpdateUserUseCase.Command(
+                manager.id(), Role.MANAGER, existing.id(), "Updated", "ByManager"));
+
+        assertThat(result.firstName()).isEqualTo("Updated");
+    }
+
+    @Test
+    void adminCanUpdateAdminAccount() {
+        User admin1 = User.create(EmailAddress.of("admin1@test.com"), "h", "A1", "Min", Role.ADMIN).user();
+        repo.save(admin1);
+        User admin2 = User.create(EmailAddress.of("admin2@test.com"), "h", "A2", "Min", Role.ADMIN).user();
+        repo.save(admin2);
+
+        UserReadModel result = handler.update(new UpdateUserUseCase.Command(
+                admin1.id(), Role.ADMIN, admin2.id(), "Updated", "ByAdmin"));
+
+        assertThat(result.firstName()).isEqualTo("Updated");
+    }
 }
